465dfd769c38ea58113e65e6a96c0cab88e6299b
misteryeo <andy.yeo@gmail.com>
2017-05-20 16:24:51 -0700

    Update bot to speak

--- index.js
+++ index.js
@@ -28,6 +28,52 @@ app.get('/webhook/', (req, res) => {
   res.send('Error, wrong token')
 })
 
+// Post data to Facebook
+app.post('/webhook/', (req, res) => {
+  let messaging_events = req.body.entry[0].messaging
+  for (let i = 0; i < messaging_events.length; i++) {
+    let event = req.body.entry[0].messaging[i]
+    let sender = event.sender.id
+    if (event.message && event.message.text) {
+      let text = event.message.text
+      if (text === 'Generic'){ 
+        console.log("welcome to chatbot")
+        //sendGenericMessage(sender)
+        continue
+      }
+      sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+    }
+    if (event.postback) {
+      let text = JSON.stringify(event.postback)
+      sendTextMessage(sender, "Postback received: " + text.substring(0, 200), token)
+      continue
+    }
+  }
+  res.sendStatus(200)
+})
+
+const token = process.env.FB_PAGE_TOKEN
+
+// Function to echo back messages
+function sendTextMessage(sender, text) {
+    let messageData = { text:text }
+    request({
+      url: 'https://graph.facebook.com/v2.6/me/messages',
+      qs: {access_token:token},
+      method: 'POST',
+    json: {
+        recipient: {id:sender},
+      message: messageData,
+    }
+  }, function(error, response, body) {
+    if (error) {
+        console.log('Error sending messages: ', error)
+    } else if (response.body.error) {
+        console.log('Error: ', response.body.error)
+      }
+    })
+}
+
 // Start server
 app.listen(app.get('port'), () => {
   console.log('running on port', app.get('port'))
