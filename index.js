6f980e13eca8fa4c553f0d81d2098e2deba4e667
misteryeo <andy.yeo@gmail.com>
2017-05-20 13:56:24 -0700

    Add initial files for bot

---
+++ index.js
@@ -0,0 +1,34 @@
+'use strict'
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
+const app = express()
+
+// Set port number to 3000
+app.set('port', (process.env.PORT || 3000))
+
+// Body parser for url encoded and application json
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
+
+app.use(bodyParser.json())
+
+// Index route
+app.get('/', (req, res) => {
+  res.send('Heya, I\'m your new chat bot!')
+})
+
+// Facebook verification
+app.get('/webhook/', (req, res) => {
+  if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
+    res.send(req.query['hub.challenge'])
+  }
+  res.send('Error, wrong token')
+})
+
+// Start server
+app.listen(app.get('port'), () => {
+  console.log('running on port', app.get('port'))
+})
