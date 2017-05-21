205239891680365418035fa22ac6852b0a6839be
misteryeo <andy.yeo@gmail.com>
2017-05-20 17:25:20 -0700

    Update message structure

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.post('/webhook/', (req, res) => {
       let text = event.message.text
       if (text === 'Generic'){ 
         console.log("welcome to chatbot")
-        //sendGenericMessage(sender)
+        sendGenericMessage(sender)
         continue
       }
       sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
@@ -69,6 +69,56 @@ function sendTextMessage(sender, text) {
     if (error) {
         console.log('Error sending messages: ', error)
     } else if (response.body.error) {
+        console.log('Error: ', response.body.error)
+      }
+    })
+}
+
+// Send structure message as cards or buttons
+function sendGenericMessage(sender) {
+    let messageData = {
+      "attachment": {
+        "type": "template",
+        "payload": {
+        "template_type": "generic",
+          "elements": [{
+          "title": "First card",
+            "subtitle": "Element #1 of an hscroll",
+            "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
+            "buttons": [{
+              "type": "web_url",
+              "url": "https://www.messenger.com",
+              "title": "web url"
+            }, {
+              "type": "postback",
+              "title": "Postback",
+              "payload": "Payload for first element in a generic bubble",
+            }],
+          }, {
+            "title": "Second card",
+            "subtitle": "Element #2 of an hscroll",
+            "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
+            "buttons": [{
+              "type": "postback",
+              "title": "Postback",
+              "payload": "Payload for second element in a generic bubble",
+            }],
+          }]
+        }
+      }
+    }
+    request({
+      url: 'https://graph.facebook.com/v2.6/me/messages',
+      qs: {access_token:token},
+      method: 'POST',
+      json: {
+        recipient: {id:sender},
+        message: messageData,
+      }
+    }, function(error, response, body) {
+      if (error) {
+        console.log('Error sending messages: ', error)
+      } else if (response.body.error) {
         console.log('Error: ', response.body.error)
       }
     })
