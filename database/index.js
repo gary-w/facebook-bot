91d920e4bc9a7a191dc6f81e63d5610a5a73cfa8
misteryeo <andy.yeo@gmail.com>
2017-05-29 12:15:11 -0700

    Update database config

--- database/index.js
+++ database/index.js
@@ -2,7 +2,7 @@ const pgp = require('pg-promise')();
 const schema = require('./schema.js');
 
 // Create connection string
-const connectionString = 'postgres://luceabsihwthuw:73b97bee32c0089548b0345ca408f6391676abbd700dbf4e9b0e549c0050fb08@ec2-54-163-246-154.compute-1.amazonaws.com:5432/daf0f5dr7q016m' || 'postgres://localhost:5432/chatbot'
+const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/chatbot'
 // Create database
 const db = pgp(connectionString);
 
