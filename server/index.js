a4e2350b23bbec66d5ed23293e11019f1478647d
misteryeo <andy.yeo@gmail.com>
2017-05-29 16:09:57 -0700

    Update server file

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
-const db = require('./database/index.js')
+const { db } = require('../database/index.js')
 const pgp = require('pg-promise')();
 const app = express()
 
