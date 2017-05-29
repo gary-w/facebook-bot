972b9d0778765b4f23f21dce9d6f15f70a4b1b29
misteryeo <andy.yeo@gmail.com>
2017-05-29 15:07:40 -0700

    Update database schema

--- database/schema.js
+++ database/schema.js
@@ -1,14 +1,14 @@
 module.exports = (db) => {
   return db.query('CREATE TABLE IF NOT EXISTS users(\
-    user_id SERIAL PRIMARY KEY, \
-    username VARCHAR(80) UNIQUE \
+    id SERIAL PRIMARY KEY, \
+    usertoken VARCHAR(80) UNIQUE \
     );')    
   .then(() => {
   return db.query('CREATE TABLE IF NOT EXISTS todo(\
     id SERIAL PRIMARY KEY, \
     item VARCHAR(500) NOT NULL, \
     status BOOLEAN NOT NULL DEFAULT FALSE, \
-    user_id INTEGER REFERENCES users (user_id) \
+    user_id INTEGER REFERENCES users (id) \
     );')    
   })
   .catch((error) => {
