65953eb70272b5271f3aa3f5ce0dd2e4f26c4e83
misteryeo <andy.yeo@gmail.com>
2017-05-29 15:53:28 -0700

    Add test structure

---
+++ spec/server/index.test.js
@@ -0,0 +1,13 @@
+const chai = require('chai');
+const server = require('../server/index.js');
+const should = chai.should();
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
+const db = require('../database/index.js')
+const pgp = require('pg-promise')();
+const app = express()
+
+describe('', function() {
+  
+})
