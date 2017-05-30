f8cb85eedf24e24af2a451d25f3a3d6dc73c2911
misteryeo <andy.yeo@gmail.com>
2017-05-29 20:16:14 -0700

    Update schema

--- database/schema.js
+++ database/schema.js
@@ -8,14 +8,12 @@ module.exports = (db) => {
     id SERIAL PRIMARY KEY, \
     item VARCHAR(500) NOT NULL, \
     status BOOLEAN NOT NULL DEFAULT FALSE, \
-    user_id INTEGER NOT NULL \
+    user_id INTEGER REFERENCES users(id) \
     );')    
   })
   .catch((error) => {
     console.log('There is an error: ', error);
   });
 };
-
-// TO DO: The foreign key was causing bugs, have removed it will look at reintroducing it when I have time
 
 
